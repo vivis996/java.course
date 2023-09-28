74b8538a3cf3990ed807c3c2554ac5ab4179ba1e
Daniel Viveros <dviveros90@gmail.com>
2023-09-27 22:24:40 -0400

    if-then statement

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Daniel");
-        System.out.print("Hello World");
+
+        var isAlien = false;
+        if (!isAlien){
+            System.out.println("It's not an alien");
+            System.out.println("And I'm scared of aliens");
+        }
     }
 }

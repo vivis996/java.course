e01b8bf6d5ab52ca021c556abbe38c099e734758
Daniel Viveros <dviveros90@gmail.com>
2023-09-29 12:20:20 -0400

    More on Methods

--- src/ExpressionStatementMore/MainChallenge.java
+++ src/ExpressionStatementMore/MainChallenge.java
@@ -3,29 +3,28 @@ package ExpressionStatementMore;
 public class MainChallenge {
     public static void main(String[] args) {
         var gameOver = true;
-        int score = 10000,
-                levelCompleted = 8,
-                bonus = 200,
-                finalScore = score;
-        calculateScore(true, 800, levelCompleted, bonus);
+        int score = 800,
+            levelCompleted = 5,
+            bonus = 100;
+
+        var highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("The highScore is " + highScore);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
-        finalScore = score;
-
-        if (gameOver) {
-            finalScore += levelCompleted * bonus;
-            System.out.println("Your final score was " + finalScore);
-        }
+        System.out.println("The nex highScore is " +
+                    calculateScore(gameOver, score, levelCompleted, bonus));
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         var finalScore = score;
         if (gameOver) {
             finalScore += levelCompleted * bonus;
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
         }
+
+        return finalScore;
     }
 }

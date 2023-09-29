c2561d25667893cba820c1cb6a3f360328dd56f1
Daniel Viveros <dviveros90@gmail.com>
2023-09-29 12:15:12 -0400

    Methods in Java

--- src/ExpressionStatementMore/MainChallenge.java
+++ src/ExpressionStatementMore/MainChallenge.java
@@ -2,14 +2,30 @@ package ExpressionStatementMore;
 
 public class MainChallenge {
     public static void main(String[] args) {
-        var newGameOver = true;
-        int newScore = 10000,
-            newLevelCompleted = 8,
-            newBonus = 200,
-            newFinalScore = newScore;
-        if ( newGameOver) {
-            newFinalScore +=  newLevelCompleted * newBonus;
-            System.out.println("Your final score was " + newFinalScore);
+        var gameOver = true;
+        int score = 10000,
+                levelCompleted = 8,
+                bonus = 200,
+                finalScore = score;
+        calculateScore(true, 800, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        finalScore = score;
+
+        if (gameOver) {
+            finalScore += levelCompleted * bonus;
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        var finalScore = score;
+        if (gameOver) {
+            finalScore += levelCompleted * bonus;
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
         }
     }
 }

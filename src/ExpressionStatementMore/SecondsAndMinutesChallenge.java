c391b7ba2d12ed1ea805dfbdfcb6748809e38ce0
Daniel Viveros <dviveros90@gmail.com>
2023-09-29 19:46:00 -0400

    Bonus Challenge Solution

--- src/ExpressionStatementMore/SecondsAndMinutesChallenge.java
+++ src/ExpressionStatementMore/SecondsAndMinutesChallenge.java
@@ -2,22 +2,37 @@ package ExpressionStatementMore;
 
 public class SecondsAndMinutesChallenge {
     public static void main(String[] args) {
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
         System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
     }
 
     private static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            return "Invalid data for seconds (" + seconds +
+                    "), must be a positive integer value";
+        }
         // Two step approach to get hours
         var minutes = seconds / 60;
-        return  getDurationString(minutes, seconds);
+        return  getDurationString(minutes, seconds % 60);
     }
 
     private static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0) {
+            return "Invalid data for minutes (" + minutes +
+                    "), must be a positive integer value";
+        }
+        if (seconds < 0 || seconds >= 59) {
+            return "Invalid data for seconds (" + minutes +
+                    "), must be between 0 and 59";
+        }
+
         var hours = minutes / 60;
 
         var remainingMinutes = minutes % 60;
-        var remainingSeconds = seconds % 60;
 
-        return hours + " h " + remainingMinutes + " m " + remainingSeconds + " s";
+        return hours + " h " + remainingMinutes + " m " + seconds + " s";
     }
 }
